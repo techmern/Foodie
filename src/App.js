e7c55aee95b091b7c41e06e06eca5b9f552bc70e
tejasvimistry4 <cherrymistry4@gmail.com>
2024-05-03 16:35:12 +0530

    third commit by tejasvi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Login_Restaurant from './Components/RegistrationRest/Login_Restaurant';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Login_User from './Components/RegistrationUser/Login_User';
 import Registration_Restaurant from './Components/RegistrationRest/Registration_Restaurant';
+import Registration_User from './Components/RegistrationUser/Registration_User';
+import User_Profile from './Components/RegistrationUser/User_Profile';
+import Update_User_Profile from './Components/RegistrationUser/Update_User_Profile';
+import Welcomepage_User from './Components/RegistrationUser/Welcomepage_User';
 
 
 function App() {
@@ -15,6 +18,10 @@ function App() {
           <Route exact path='' element={<Registration_Restaurant />} />
           <Route exact path='/loginRestaurant' element={<Login_Restaurant />} />
           <Route exact path='/userlogin' element={<Login_User />} />
+          <Route exact path='/userregister' element={<Registration_User />} />
+          <Route exact path='/userprofile' element={<User_Profile />} />
+          <Route exact path='/updateuser' element={<Update_User_Profile />} />
+          <Route exact path='/welcomeuser' element={<Welcomepage_User />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/Components/RegistrationUser/Login_User.jsx
+++ src/Components/RegistrationUser/Login_User.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
-import './style2.css'
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './User.css'
+import { useNavigate } from 'react-router-dom';
+
+import axios from 'axios';
 
 function Login_User() {
-  return (
-    <div>
+    const navigate = useNavigate()
+
+
+    const [userlogin, setUserlogin] = useState({
+        'Email_Id': '',
+        'Password': '',
+    })
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target
+        setUserlogin({
+            ...userlogin,
+            [name]: value
+        })
+    }
+
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+
+        // Check for empty fields
+        if (userlogin.Email_Id.trim() === '' || userlogin.Password.trim() === '') {
+            setErrors({
+                Email_Id: 'Email field cannot be blank',
+                Password: 'Password field cannot be blank',
+            });
+            return;
+
+        }
+
+        try {
+            const response = await axios.post('http://localhost:5000/user/userlogin', userlogin);
+            console.log(response.data);
+            const { data } = response;
+            console.log('Response data:', data);
+
+
+            localStorage.setItem('User', JSON.stringify(data.userData));
+            console.log('Stored user data:', data.userData);
+
+
+            if (data.loginsts === 0) {
+                alert('Email not found');
+
+            } else if (data.loginsts === 1) {
+                alert('Password is Incorrect');
+                
+            } else if (data.loginsts === 2) {
+                const userData = data.userData;
+                console.log('Stored user data:', userData);
+                localStorage.setItem('User', JSON.stringify(userData));
+                navigate('/welcomeuser');
+                alert('Login successful!');
+            }
+
+
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const [errors, setErrors] = useState({});
+
+    const handleInputBlur = (e) => {
+        const { name, value } = e.target;
+
+        if (value.trim() === '') {
+            setErrors({
+                ...errors,
+                [name]: `${name} field can not be blank`,
+            });
+
+
+        }
+    };
+
+
+
+    return (
         <div className='bg-image' style={{ minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <div className='form' style={{ width: '100%' }}>
                 <div className='container'>
@@ -14,11 +94,13 @@ function Login_User() {
                             </div>
                             <h1 className="text-center mb-4">Login</h1>
                             <div className="mb-4">
-                                <input type="email" className="form-control" placeholder="Enter email" id="exampleInputEmail1" aria-describedby="emailHelp" name='Email_Id'  />
+                                <input type="email" className="form-control" placeholder="Enter email" id="exampleInputEmail1" aria-describedby="emailHelp" name='Email_Id' onChange={handleInputChange} onBlur={handleInputBlur} />
+                                {errors.Email_Id && <span className="text-danger">{errors.Email_Id}</span>}
 
                             </div>
                             <div className="mb-3">
-                                <input type="text" className="form-control" placeholder="Enter Password" id="exampleInputPassword1" name='Password' />
+                                <input type="text" className="form-control" placeholder="Enter Password" id="exampleInputPassword1" name='Password' onChange={handleInputChange} onBlur={handleInputBlur} />
+                                {errors.Password && <span className="text-danger">{errors.Password}</span>}
 
                             </div>
                             <div className="text-left mb-3">
@@ -26,7 +108,7 @@ function Login_User() {
                             </div>
 
                             <div className="mb-3">
-                                <button type="submit" className="btn btn-primary mx-auto d-block" style={{ width: '391px' }} >Login</button>
+                                <button type="submit" className="btn btn-primary mx-auto d-block" style={{ width: '391px' }} onClick={handleFormSubmit}>Login</button>
                             </div>
                             <div className="mb-3">
                                 <p style={{ textAlign: 'center' }}>OR</p>
@@ -43,8 +125,7 @@ function Login_User() {
             </div>
 
         </div>
-    </div>
-  )
+    )
 }
 
 export default Login_User
